fe7bef3329c38c88ccc2530b6dcd7f1e77966161
Papillon6814 <so1rou.k@gmail.com>
2021-06-23 21:54:23 +0900

    underscore

--- oban_sample/lib/oban_sample/cron.ex
+++ oban_sample/lib/oban_sample/cron.ex
@@ -2,8 +2,8 @@ defmodule ObanSample.Cron do
   use Oban.Worker
 
   @impl Oban.Worker
-  def perform(job) do
-    #IO.inspect("minute worker")
+  def perform(_job) do
+    IO.inspect("minute worker")
     :ok
   end
 end
